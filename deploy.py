e661ae65593af483b588fdb7a14defd2ebf92af3
Michael Ruberto <rubertmi@pascack.org>
2019-02-01 09:29:24 -0500

    Add Page Two

--- deploy.py
+++ deploy.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
    return render_template("index.html")
+
+@app.route("/page_two")
+def page_two():
+   return render_template("page_two.html")
    
 if __name__ == "__main__":
    app.run(debug=True)
